fabcac668885227024691511bbad76ba37863023
ISSOtm <eldredhabert0@gmail.com>
2019-05-03 23:38:11 +0200

    Write shallow testing main code Tests: - Duplicated insertions ("quarte" twice) - Inserting over a partial self ("quartet" over "quarte") - Inserting over all-created nodes ("quartile" over "quartiles") - Inserting over a "diverging path" ("quartiles" over "quarte") - Inserting over something completely unrelated ("bien" over "quatre")

--- src/main.c
+++ src/main.c
@@ -1,8 +1,18 @@
 
-#include "tree.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include "dict.h"
 
 
 int main(int argc, char* argv[]) {
+    Tree_t * dictionary = NULL;
+    char const * const words[] = {
+        "quatre", "bien", "quarte", "quartiles", "bien", "bielle", "quartile", "quarte", "quartet", "quantifiable"
+    };
+    insert_words(&dictionary, words, sizeof(words) / sizeof(char *));
+
+    list_words(dictionary);
+
     (void)argc;
     (void)argv;
     return 0;
